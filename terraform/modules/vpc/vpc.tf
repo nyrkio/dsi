fc0a134ee051c173d9cc2c78e9878559b332b663
Henrik Ingo <henrik@nyrk.io>
2024-07-01 14:25:57 +0300

    Re-enable the firewall in Terraform vpc configuration
    The MongoDB ransomware industry is impressive: Can't even
    load the data before they already deleted it. This is good,
    it means you can't lose anything of value, because they will hit
    you before you're even online.

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -87,10 +87,16 @@ resource "aws_security_group" "default" {
     vpc_id = aws_vpc.main.id
 
     ingress {
+        from_port = 22
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [var.vpc_cidr_block]
     }
     egress {
         from_port = 0
